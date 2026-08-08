80272c96bc7919fa50bc33170aadfd0a0ca334fc
Edward <cololacalempira5@gmail.com>
2026-08-08 13:16:51 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const uploadRoute = require('./routes/upload');
 const serveRoute = require('./routes/serve');
+const { router: myFilesRoute, cleanupExpired } = require('./routes/myfiles');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,9 +14,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/upload', uploadRoute);
 app.use('/cdn', serveRoute);
+app.use('/my-files', myFilesRoute);
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
+
+cleanupExpired();
+setInterval(cleanupExpired, 15 * 60 * 1000);
 
 app.listen(PORT, () => console.log(`Duan CDN corriendo en el puerto ${PORT}`));
